4255f154b6cb239426b8e299df3a777db521c649
Tom Canham <tomc@ricksteves.com>
2013-05-01 13:22:56 -0700

    Tweaks

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,8 @@
-class User < ActiveRecord::Base
+require 'ricksteves/auth/client/user'
+
+class User < RickSteves::Auth::Client::User
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :trackable, :omniauthable
-
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email
-
-  def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
-      user.email = auth.info.info.email
-    end
-  end
-
-  def password_required?
-    # this lets us create an account with no password from OAuth
-    false
-  end
 end
